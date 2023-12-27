55e4c4f158155cc0a044ed6dedecccb1873e4609
h3b4r1 <dtaylor@silvacloud.net.au>
2023-12-27 13:05:19 +0000

    minor update

--- test_app.py
+++ test_app.py
@@ -5,11 +5,21 @@ import argparse
 def main():
     '''some code to show the use of argparse by dispalying the linux system information'''
     arg_parser = argparse.ArgumentParser(description='Display the system information')
-    arg_parser.add_argument('-o', '--os', action='store_true', help='Display the OS information')
-    arg_parser.add_argument('-m', '--memory', action='store_true', help='Display the memory information')
-    arg_parser.add_argument('-c', '--cpu', action='store_true', help='Display the CPU information')
-    arg_parser.add_argument('-n', '--network', action='store_true', help='Display the network information')
-    arg_parser.add_argument('-a', '--all', action='store_true', help='Display all the information')
+    arg_parser.add_argument(
+        '-o', '--os', action='store_true', help='Display the OS information'
+        )
+    arg_parser.add_argument(
+        '-m', '--memory', action='store_true', help='Display the memory information'
+        )
+    arg_parser.add_argument(
+        '-c', '--cpu', action='store_true', help='Display the CPU information'
+        )
+    arg_parser.add_argument(
+        '-n', '--network', action='store_true', help='Display the network information'
+        )
+    arg_parser.add_argument(
+        '-a', '--all', action='store_true', help='Display all the information'
+        )
     args = arg_parser.parse_args()
     if len(sys.argv) != 2:
         arg_parser.print_help()
@@ -18,7 +28,7 @@ def main():
         os.system('uname -a')
     if args.memory:
         os.system('free -m')
-    if args.cpu:    
+    if args.cpu:
         os.system('lscpu')
     if args.network:
         os.system('ifconfig')
